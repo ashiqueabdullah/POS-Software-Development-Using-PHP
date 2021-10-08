8cd20adfb280c9e35ab08d56e2f7784d17733c8a
ashique13 <ashiqueashique465@gmail.com>
2021-10-08 12:40:11 +0600

    pie and bar chart

--- js/main.js
+++ js/main.js
@@ -1,4 +1,60 @@
 $(document).ready(function () {
+	var ctx = document.getElementById('myChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['1 Oct', '2 Oct', '3 Oct', '4 Oct', '5 Oct', '6 Oct'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 13, 15, 18, 16],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 
+	var ctx2 = document.getElementById('myChart2').getContext('2d');
+	var myChart = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: ['Income', ' Expenses'],
+        datasets: [{
+            label: '# of Votes',
+            data: [15, 19],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    
+});
 });
